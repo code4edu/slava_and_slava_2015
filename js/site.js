911e4775b1c7c4a28b7cd39e3ecbea37c9c17071
Zheltov Slava <oookordial@ya.ru>
2015-12-19 10:34:39 +0300

    upd
    add jquery and cookie JS

---
+++ js/site.js
@@ -0,0 +1,13 @@
+function getCookie(name) {
+  var matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+function setCookie (name, value, expires, path, domain, secure) {
+	document.cookie = name + "=" + escape(value) +
+		((expires) ? "; expires=" + expires : "") +
+		((path) ? "; path=" + path : "") +
+		((domain) ? "; domain=poedim.csit.pro") +
+		((secure) ? "; secure" : "");
+}
