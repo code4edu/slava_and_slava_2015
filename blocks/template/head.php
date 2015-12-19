a5ae2c873f58142ad60934c89a7fcc33c702c193
Zheltov Slava <oookordial@ya.ru>
2015-12-19 12:35:31 +0300

    head changes

--- blocks/template/head.php
+++ blocks/template/head.php
@@ -2,12 +2,15 @@
 <html lang="ru-Ru">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Поедим</title>
+    <title>Закажи еду в два клика и забери в столовой без очереди! Поедим</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="keywords" content="Поедим">
     <meta name="description" content="Поедим">
     <link type="image/ico" href="/img/favicon.ico" rel="shortcut icon"/>
     <link rel="stylesheet" href="/css/style.css">
     <script src="/js/jquery.js" type="text/javascript"></script>
+    <link type="image/ico" href="/img/favicon.ico" rel="shortcut icon"/>
+    <link rel="stylesheet" href="/css/style.css">
+
     <script src="/js/site.js" type="text/javascript"></script>
 </head>
